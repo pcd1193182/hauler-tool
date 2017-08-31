de12607cd891f3385523a4bd6ede8baa076b37bc
Paul Dagnelie <paulcd2000@gmail.com>
2017-08-30 18:56:13 -0700

    don't change the type dummy

--- fittings.py
+++ fittings.py
@@ -15,8 +15,12 @@ def process_resp(esiapp, esiclient, resp):
         resp = esiclient.request(op)
         if resp.status != 200:
             raise APIException('', resp.status, resp.data)
-        fit_list.get(resp.data.name, []) += fit
+        
         print resp.data.name
+        if resp.data.name in fit_list:
+            fit_list[resp.data.name] += fit
+        else:
+            fit_list[resp.data.name] = [fit]
     
     return OrderedDict(sorted(ship_list.items(), key=lambda t: t[0]))
         
