b4729f4646f1254883506fe0ad7e8a1552cb3fde
Paul Dagnelie <paulcd2000@gmail.com>
2017-08-30 20:45:21 -0700

    js debug

--- static/info.js
+++ static/info.js
@@ -1,7 +1,9 @@
 $('tr.ShipType').click(function(){
-    console.log("test")
+    console.log("test");
     $(this).find('span').text(function(_, value){return value=='-'?'+':'-'});
     $(this).nextUntil('tr.ShipType').css('display', function(i,v){
 	return this.style.display === 'table-row' ? 'none' : 'table-row';
     });
 });
+$('tr').click(function() { console.log("test1"); });
+$('th').click(function() { console.log("test2"); });
