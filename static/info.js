8bab85d18d262b78b821d9e33230a4775276bba0
Paul Dagnelie <paulcd2000@gmail.com>
2017-08-31 16:47:24 -0700

    need ajax yo

--- static/info.js
+++ static/info.js
@@ -21,20 +21,20 @@ $('.ShipName').click(function() {
 $('#fit_form').submit(function(ev) {
     ev.preventDefault(); // to stop the form from submitting
     if (selected_fit === undefined) {
-	$("#error_text").text("Please select fit");
-	$("#success_text").text(""); 
+	$("#report_text").css('color', 'red');
+	$("#report_text").text("Please select fit");
 	return;
     }
     var evep_url = $('[name="evep_url"');
     var cargo_size = $('[name="cargo_size"');
     if (evep_url.val() === '') {
-	$("#error_text").text("Error, empty url");
-	$("#success_text").text(""); 
+	$("#report_text").css('color', 'red');
+	$("#report_text").text("Error, empty url");
 	return;
     }
     if (cargo_size.val() === '') {
-	$("#error_text").text("Error, cargo size");
-	$("#success_text").text(""); 
+	$("#report_text").css('color', 'red');
+	$("#report_text").text("Error, cargo size");
 	return;
     }
     var values = {
@@ -42,14 +42,22 @@ $('#fit_form').submit(function(ev) {
 	'evep_url' : evep_url.val(),
 	'cargo_size' : cargo_size.val()
     };
+    $("#report_text").css('color', 'gray');
+    $("#report_text").text("Working...");
     $.ajax({
 	type: "POST",
 	url: "/gen_fit",
 	data: JSON.stringify(values),
 	contentType: "application/json; charset=utf-8",
 	dataType: "json",
-	success: function(data) {$("#success_text").text("Success! " + data.msg); $("#error_text").text(""); console.log("success");},
-	error: function(errMsg) {$("#error_text").text("Error " + errMsg.status + ": " + JSON.parse(errMsg.responseText).msg); $("#success_text").text(""); console.log("failure");},
+	success: function(data) {
+	    $("#report_text").text("Success! " + data.msg);
+	    $("#report_text").css('color', 'blue');
+	},
+	error: function(errMsg) {
+	    $("#report_text").text("Error " + errMsg.status + ": " + JSON.parse(errMsg.responseText).msg);
+	    $("#report_text").css('color', 'red');
+	},
 	complete: function() {console.log("complete");}
     });
 });
