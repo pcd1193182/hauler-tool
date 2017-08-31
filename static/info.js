b73338ad85da4c48270d9cdc62bac8dc37983776
Paul Dagnelie <paulcd2000@gmail.com>
2017-08-30 20:32:39 -0700

    test in js

--- static/info.js
+++ static/info.js
@@ -1,4 +1,5 @@
 $('tr.ShipType').click(function(){
+    console.log("test")
     $(this).find('span').text(function(_, value){return value=='-'?'+':'-'});
     $(this).nextUntil('tr.ShipType').css('display', function(i,v){
 	return this.style.display === 'table-row' ? 'none' : 'table-row';
