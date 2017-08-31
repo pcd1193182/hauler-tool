dfe951d46e4821af538bf222b21d8a099966bace
Paul Dagnelie <paulcd2000@gmail.com>
2017-08-30 21:17:15 -0700

    try with slide way instead

--- static/info.js
+++ static/info.js
@@ -2,9 +2,10 @@ console.log("is this thing on");
 $('.ShipType').click(function(){
     console.log("test");
     $(this).find('span').text(function(_, value){return value=='-'?'+':'-'});
-    $(this).nextUntil('tr.ShipType').css('display', function(i,v){
-	return this.style.display === 'table-row' ? 'none' : 'table-row';
-    });
+    $(this).nextUntil('tr.ShipType').slideToggle(100, function(){
+    });//css('display', function(i,v){
+//	return this.style.display === 'table-row' ? 'none' : 'table-row';
+//    });
 });
 $('tr').click(function() { console.log("test1"); });
 $('th').click(function() { console.log("test2"); });
