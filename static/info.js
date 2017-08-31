dfc05e2fa4fde461cbd41fb40b036cc4d8ffc602
Paul Dagnelie <paulcd2000@gmail.com>
2017-08-30 23:01:20 -0700

    CSS approach

--- static/info.js
+++ static/info.js
@@ -2,14 +2,8 @@ console.log("is this thing on");
 $('.ShipType').click(function(){
     console.log("test");
     $(this).find('span').text(function(_, value){return value=='-'?'+':'-'});
-    $(this).nextUntil('tr.ShipType').slideToggle(100, function(){
-    });//css('display', function(i,v){
-//	return this.style.display === 'table-row' ? 'none' : 'table-row';
-//    });
-});
-$('.ShipType').ready(function() {
-    $(this).find('span').text(function(_, value){return value=='-'?'+':'-'});
-    $(this).nextUntil('tr.ShipType').slideToggle(1, function(){
+    $(this).nextUntil('tr.ShipType').css('display', function(i,v){
+	return this.style.display === 'table-row' ? 'none' : 'table-row';
     });
 });
 $('tr').click(function() { console.log("test1"); });
